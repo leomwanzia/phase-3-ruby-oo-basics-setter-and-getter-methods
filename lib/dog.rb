4bfeb8e4096fdfb1ebe028d9962852ef7ca90200
Leo <leomutungi19@gmail.com>
2022-10-21 00:26:11 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-
+    #name
+    #setter
+    def name=(name)
+        @name = name
+    end
+    #getter
+    def name
+        @name
+    end
+    #breed
+    #setter
+    def breed=(breed)
+        @breed = breed
+    end
+    #getter
+    def breed
+        @breed
+    end
 end
